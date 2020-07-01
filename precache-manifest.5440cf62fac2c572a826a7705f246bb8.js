00404fc823c2804887ddcfc966369f485e5b34dd
Ahmed Tarek <a7md.tare2@gmail.com>
2020-07-01 17:15:18 +0200

    project files

---
+++ precache-manifest.5440cf62fac2c572a826a7705f246bb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77478351b12223f6a8f2e0601eb796f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbf99331c0476cda0d1b",
+    "url": "/static/css/main.970c4ae9.chunk.css"
+  },
+  {
+    "revision": "a6d8074105e4bc74b9c7",
+    "url": "/static/js/2.4d06c6ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d06c6ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf99331c0476cda0d1b",
+    "url": "/static/js/main.c1d3ee54.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
